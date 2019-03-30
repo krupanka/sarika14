82878f2f0ce01d410d6bf3b9c6b440024854d71d
sarika <sarika14@gmail.com>
2019-03-30 09:45:00 +0530

    update new

---
+++ src/app/components/crm/crm-po/po-entry/po-entry-requester-selection/po-entry-requester-selection.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
+import{ Router,ActivatedRoute } from '@angular/router';
+import{ HttpResponse} from '@angular/common/http';
+import { PoEntryServicesService } from 'src/app/scrvices/crm/po/po-entry/po-entry-services.service';
+import { MatTableDataSource,MatSort,MatPaginator } from '@angular/material';
+
+
+
+
+@Component({
+  selector: 'app-po-entry-requester-selection',
+  templateUrl: './po-entry-requester-selection.component.html',
+  styleUrls: ['./po-entry-requester-selection.component.css']
+})
+export class PoEntryRequesterSelectionComponent implements OnInit {
+  quotation_id="";
+  products=[];
+  quotation_lineitems = [];
+ 
+ 
+  constructor(private poEntryServicesService:PoEntryServicesService,
+    private router:Router,private route:ActivatedRoute, private formBuilder: FormBuilder) { }
+    listData: MatTableDataSource<any>;
+    displayedColumns: string[] = ['product_title', 'description', 'model', 'brand'];
+    
+
+    ngOnInit() {
+      let quot_id="ec57ccf3-cc37-400c-b699-16b25c045111";
+      this.PoProductSelectionPage(quot_id)
+    }
+    PoProductSelectionPage(quot_id){
+      console.log(quot_id);
+      this.poEntryServicesService.getPoProductSelectionList(quot_id).subscribe((data)=>{  // get method
+       this.products=data;
+        console.log(this.products);  
+        
+    })
+  }
+ 
+  }  
+
+
+  
+
